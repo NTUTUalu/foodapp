760c8c82f5b2b02b1d528563266d5264c84c0b19
Ntutu Sekonyela <n.sekonyela@alustudent.com>
2023-11-03 22:15:48 +0200

    created some sub-directories

---
+++ pages/offers/offers.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function FirstPost() {
+    return (
+        <>
+          <h1>offers page</h1>
+          <Link href='/'>return to home page</Link>
+        </>
+      );
+  }
+  
+  
---
+++ pages/services/services.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>Services page</h1>;
+      <Link href='/'>return to home page</Link>
+    </>
+  );
+}
