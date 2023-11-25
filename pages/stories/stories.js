1e500b65a4d0271f8c56654039c55f036bc876fe
Ntutu Sekonyela <n.sekonyela@alustudent.com>
2023-11-25 17:17:49 +0200

    fixed our stories segment, wow what a productive day

--- pages/stories/stories.js
+++ pages/stories/stories.js
@@ -1,18 +1,19 @@
 import Link from "next/link";
 import Image from "next/image";
 import b from '../contact-page/contact' 
+import Styles from './stories.module.css'
 
 export default function Stories() {
   return (
     <>
-<div className="wrapper  flex w-full bg-blue-500 h-screen">
-    <div className="left flex flex-col bg-pink-500 w-1/2">
-        <h4>OUR STORY & SERVICES</h4>
-        <h1>Our Culinary Journey And Services</h1>
-        <p>Rooted in passion, we curate unforgettable dining experiences and offer exceptional services, blending culinary artistry with warm hospitality.</p>
+<div className="wrapper  flex w-full  px-20 h-screen">
+    <div className="left flex flex-col  w-1/2 mt-32">
+        <h4 className="text-red-500 font-medium mb-6">OUR STORY & SERVICES</h4>
+        <h1  className="text-slate-950 font-bold text-4xl w-4/6 mb-6">Our Culinary Journey And Services</h1>
+        <p className="w-4/6 text-slate-500 font-normal mb-8">Rooted in passion, we curate unforgettable dining experiences and offer exceptional services, blending culinary artistry with warm hospitality.</p>
         <div className="buttons">
         <Link href="/contact-page/contact" className="">
-        <button className="basis-1 flex  text-white text-base font-normal bg-green-500 px-12">
+        <button className="basis-1 flex  text-white text-base font-normal rounded-3xl bg-green-400 py-2 px-8">
         
           Explore
         </button>
@@ -22,49 +23,62 @@ export default function Stories() {
         </div>
     </div>
     <div className='right w-1/2 grid grid-cols-2 gap-4'>
-<div className="Catering bg-orange-400">
-        
-          <Image
-            src="/play.png"
+<div className="Catering flex justify-center items-center">
+        <div className={`${Styles.CardShadow} bg-slate-50 w-3/4 h-4/6 rounded-xl flex flex-col p-6 justify-evenly items-center`} >
+        <Image
+            src="/catering.png"
             alt="Vercel Logo"
             className="dark:invert"
-            width={35}
-            height={35}
+            width={40}
+            height={40}
             priority
           />
-          <h3>Catering</h3>
-          <p>Delight your guests with our flavors and  presentation</p>
+          <h3  className="font-bold text-base text-green-600 " >Catering</h3>
+          <p className="text-center text-slate-500">Delight your guests with our flavors and  presentation</p>
         </div>
-<div className="Fast-Delivery bg-orange-400">   <Image
-            src="/play.png"
+          
+        </div>
+<div className="Fast-Delivery  flex justify-center items-center">  
+<div className={`${Styles.CardShadow} bg-slate-50 w-3/4 h-4/6 rounded-xl flex flex-col p-6 justify-evenly items-center`}>
+<Image
+            src="/delivery.png"
             alt="Vercel Logo"
             className="dark:invert"
-            width={35}
-            height={35}
+            width={55}
+            height={45}
             priority
           />
-          <h3>FAST DELIVERY</h3>
-          <p>We deliver your order promptly to your door</p></div>
-<div className="Online-Ordering bg-orange-400">   <Image
-            src="/play.png"
+          <h3  className="font-bold text-base text-green-600 " >FAST DELIVERY</h3>
+          <p className="text-center text-slate-500">We deliver your order promptly to your door</p></div>
+</div>
+<div className="Online-Ordering flex justify-center items-center">   
+<div className={`${Styles.CardShadow} bg-slate-50 w-3/4 h-4/6 rounded-xl flex flex-col p-6 justify-evenly items-center`}>
+<Image
+            src="/online.png"
             alt="Vercel Logo"
             className="dark:invert"
-            width={35}
-            height={35}
+            width={40}
+            height={40}
             priority
           />
-          <h3>ONLINE ORDERING</h3>
-          <p>Delight your guests with our flavors and  presentation</p></div>
-<div className="Gift-Cards bg-orange-400">   <Image
-            src="/play.png"
+          <h3  className="font-bold text-base text-green-600 ">ONLINE ORDERING</h3>
+          <p className="text-center text-slate-500">Delight your guests with our flavors and  presentation</p>
+</div>
+</div>
+<div className="Gift-Cards  flex justify-center items-center"> 
+<div className={`${Styles.CardShadow} bg-slate-50 w-3/4 h-4/6 rounded-xl flex flex-col p-6 justify-evenly items-center`}>
+<Image
+            src="/gift.png"
             alt="Vercel Logo"
             className="dark:invert"
-            width={35}
-            height={35}
+            width={40}
+            height={40}
             priority
           />
-          <h3>GIFT CARDS</h3>
-          <p>Give the gift of exceptional dining with Foodi Gift Cards</p></div>
+          <h3 className="font-bold text-base text-green-600 ">GIFT CARDS</h3>
+          <p className="text-center text-slate-500">Give the gift of exceptional dining with Food Gift Cards</p>
+</div>
+</div>
     </div>
 
 </div>
