d1c72e78a45079b2e98736dc405d234356df3c4a
Ntutu Sekonyela <n.sekonyela@alustudent.com>
2023-11-26 10:43:01 +0200

    worked on popular dishes and made cards for food

--- pages/popularCategories/popularDishes.js
+++ pages/popularCategories/popularDishes.js
@@ -1,25 +1,91 @@
 import Link from "next/link";
 import Image from "next/image";
-import b from '../contact-page/contact' 
+import b from "../contact-page/contact";
+import Styles from './populardishes.module.css'
 
 export default function PopularDishes() {
   return (
     <>
-<div className="wrapper  flex flex-col w-full bg-blue-300 h-96">
-    <div className="description flex flex-col bg-pink-200 h-1/3">
-        <h1>CUSTOMER FAVORITES</h1>
-        <p>Popular Categories</p>
+      <div className="wrapper  flex flex-col w-full h-96 px-20 text-center">
+        <div className="description flex flex-col   h-1/3 text-slate-500">
+          <h1 className="text-red-500 font-medium">CUSTOMER FAVORITES</h1>
+          <p className="text-4xl font-bold text-slate-700">
+            Popular Categories
+          </p>
+        </div>
+        <div className="right w-full  grid  grid-cols-4 gap-4 h-3/5 p">
+          <div className="card_1 flex justify-center  items-center">
         
-    </div>
-    <div className='right w-full grid grid-cols-4 gap-4 h-3/5'>
-        <div className="card_1 bg-orange-200"><h5>food card</h5></div>
-        <div className="card_2 bg-orange-200"><h5>food card</h5></div>
-        <div className="card_3 bg-orange-200"><h5>food card</h5></div>
-        <div className="card_4 bg-orange-200"><h5>food card</h5></div>
-
-    </div>
-
-</div>
+          <div className={`${Styles.CardShadow} bg-slate-50 w-5/6 h-full rounded-xl flex flex-col p-6 justify-evenly items-center`}>
+          <div className=" mr-2  bg-blue-100 flex justify-items-center items-center rounded-full p-2">
+              <Image
+                src="/burger.png"
+                alt="Vercel Logo"
+                className="dark:invert rounded-full"
+                width={120}
+                height={30}
+                priority
+              />
+            </div>
+            <h5 className="font-bold">food card</h5>
+            <p>(86 dishes)</p>
+          </div>
+           
+          </div>
+          <div className="card_1 flex justify-center items-center ">
+        
+        <div className={`${Styles.CardShadow} bg-slate-50 w-5/6 h-full rounded-xl flex flex-col p-6 justify-evenly items-center`}>
+        <div className=" mr-2  bg-blue-100 flex justify-items-center items-center rounded-full p-2">
+            <Image
+              src="/sandwich.png"
+              alt="Vercel Logo"
+              className="dark:invert rounded-full"
+              width={120}
+              height={30}
+              priority
+            />
+          </div>
+          <h5 className="font-bold">food card</h5>
+          <p>(86 dishes)</p>
+        </div>
+         
+        </div><div className="card_1 flex justify-center items-center">
+        
+        <div className={`${Styles.CardShadow} bg-slate-50 w-5/6 h-full rounded-xl flex flex-col p-6 justify-evenly items-center`}>
+        <div className=" mr-2  bg-blue-100 flex justify-items-center items-center rounded-full p-2">
+            <Image
+              src="/icecream.png"
+              alt="Vercel Logo"
+              className="dark:invert rounded-full"
+              width={120}
+              height={30}
+              priority
+            />
+          </div>
+          <h5 className="font-bold">food card</h5>
+          <p>(86 dishes)</p>
+        </div>
+         
+        </div><div className="card_1 flex justify-center items-center">
+        
+        <div className={`${Styles.CardShadow} bg-slate-50 w-5/6 h-full rounded-xl flex flex-col p-6 justify-evenly items-center`}>
+        <div className=" mr-2  bg-blue-100 flex justify-items-center items-center rounded-full p-1">
+            <Image
+              src="/juice.png"
+              alt="Vercel Logo"
+              className="dark:invert rounded-full "
+              width={120}
+              height={10}
+              priority
+            />
+          </div>
+          <h5 className="font-bold">food card</h5>
+          <p>(86 dishes)</p>
+        </div>
+         
+        </div>
+        </div>
+      </div>
     </>
   );
 }
