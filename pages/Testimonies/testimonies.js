bf749bda68996d48b398c67210630b5c8863f265
Ntutu Sekonyela <n.sekonyela@alustudent.com>
2023-11-26 11:45:03 +0200

    started working on the testimonies section creating some layout

--- pages/Testimonies/testimonies.js
+++ pages/Testimonies/testimonies.js
@@ -5,11 +5,9 @@ import b from '../contact-page/contact'
 export default function Testimonies() {
   return (
     <>
-<div className="wrapper  flex w-full bg-blue-500 h-screen">
-<div className='right w-1/2'>
-<h5>insert image here</h5>
-    </div>
-    <div className="left flex flex-col bg-pink-500 w-1/2">
+<div className="wrapper  flex w-full  h-screen px-20">
+
+    <div className="left flex flex-col  w-1/2">
         <h1>Testimonies</h1>
         <p>What Our Customers Say About Us </p>
         <p>“I had the pleasure of dining at Foodi last night, and I'm still raving about the experience! The attention to detail in presentation and service was impeccable”</p>
@@ -36,7 +34,23 @@ export default function Testimonies() {
      
         </div>
     </div>
-  
+    <div className="left flex flex-col  w-1/2 mt-32">
+          <h4 className="text-red-500 font-medium mb-6">
+            TESTIMONIES
+          </h4>
+          <h1 className="text-slate-950 font-bold text-4xl w-4/6 mb-6">What Our Customers Say About Us
+          </h1>
+          <p className="w-4/6 text-slate-500 font-normal mb-8">
+          “I had the pleasure of dining at Foodi last night, and I'm still raving about the experience! The attention to detail in presentation and service was impeccable”
+          </p>
+          <div className="buttons">
+            <Link href="/contact-page/contact" className="">
+              <button className="basis-1 flex  text-white text-base font-normal rounded-3xl bg-green-400 py-2 px-8">
+                Explore
+              </button>
+            </Link>
+          </div>
+        </div>
 
 </div>
     </>
