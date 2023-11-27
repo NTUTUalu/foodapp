d43b68ae2dcc62ef05033778087f98d8f88058f8
Ntutu Sekonyela <n.sekonyela@alustudent.com>
2023-11-27 22:10:40 +0200

    worked on the testimonial page and made ui modifications

--- pages/Testimonies/testimonies.js
+++ pages/Testimonies/testimonies.js
@@ -7,48 +7,63 @@ export default function Testimonies() {
     <>
 <div className="wrapper  flex w-full  h-screen px-20">
 
-    <div className="left flex flex-col  w-1/2">
-        <h1>Testimonies</h1>
-        <p>What Our Customers Say About Us </p>
-        <p>“I had the pleasure of dining at Foodi last night, and I'm still raving about the experience! The attention to detail in presentation and service was impeccable”</p>
-        <div className="customer feedback">
-      <div className="images">
-
-      </div>
-      <div className="right-text flex flex-col">
-    <h1>Customer Feedback</h1>
-    <div className="flex">
+    <div className="left flex flex-col  w-1/2 mt-20  justify-center">
+        
+     
+   
+     
+ 
+    <div className="flex justify-center   ">
     <Image
-            src="/foodlogo.png"
+            src="/chef.png"
             alt="Vercel Logo"
             className="dark:invert"
-            width={25}
+            width={570}
             height={25}
             priority
           />
-          <h6>4.9</h6>
-          <p>(18.6k Reviews)</p>
+        
     </div>
-      </div>
+   
   
      
-        </div>
+     
     </div>
-    <div className="left flex flex-col  w-1/2 mt-32">
+    <div className="left flex flex-col   w-1/2 mt-32">
           <h4 className="text-red-500 font-medium mb-6">
-            TESTIMONIES
+            TESTIMONIALS
           </h4>
-          <h1 className="text-slate-950 font-bold text-4xl w-4/6 mb-6">What Our Customers Say About Us
+          <h1 className="text-slate-950 font-bold text-5xl w-5/6 mb-8">What Our Customers Say About Us
           </h1>
           <p className="w-4/6 text-slate-500 font-normal mb-8">
           “I had the pleasure of dining at Foodi last night, and I'm still raving about the experience! The attention to detail in presentation and service was impeccable”
           </p>
-          <div className="buttons">
-            <Link href="/contact-page/contact" className="">
-              <button className="basis-1 flex  text-white text-base font-normal rounded-3xl bg-green-400 py-2 px-8">
-                Explore
-              </button>
-            </Link>
+          <div className="customerFeedback flex">
+            <div className="Images  flex items-end">
+            <Image
+            src="/people.png"
+            alt="Vercel Logo"
+            className="dark:invert mr-2 h-10 "
+            width={100}
+            height={10}
+            priority
+          />
+            </div>
+            <div className="Rating flex flex-col">
+<h1 className="font-bold">Customer Feedback</h1>
+<div className="flex mt-1">
+   <Image
+            src="/rating.png"
+            alt="Vercel Logo"
+            className="dark:invert mr-2"
+            width={20}
+            height={10}
+            priority
+          /><span className="font-semibold mr-1">4.9</span>
+          (18.6k Reviews)
+    
+</div>
+            </div>
           </div>
         </div>
 
