08a2e8d04cb543a92b93cdbaf5f73e8e4a4a9e68
Ntutu Sekonyela <n.sekonyela@alustudent.com>
2023-12-07 14:04:37 +0200

    worked on the popular dishes segment made medium screen responsiveness

--- pages/popularCategories/popularDishes.js
+++ pages/popularCategories/popularDishes.js
@@ -6,17 +6,17 @@ import Styles from './populardishes.module.css'
 export default function PopularDishes() {
   return (
     <>
-      <div className="wrapper  flex flex-col w-full h-96 px-20 text-center">
+      <div className="wrapper  flex flex-col w-full h-96 px-20 text-center max-md:px-10">
         <div className="description flex flex-col   h-1/3 text-slate-500">
           <h1 className="text-red-500 font-medium">CUSTOMER FAVORITES</h1>
           <p className="text-4xl font-bold text-slate-700">
             Popular Categories
           </p>
         </div>
-        <div className="right w-full  grid  grid-cols-4 gap-4 h-3/5 p">
-          <div className="card_1 flex justify-center  items-center">
+        <div className="right w-full  grid  grid-cols-4 gap-4 h-3/5 bg-blue-200 max-md:">
+          <div className="card_1 flex justify-center  items-center bg-blue-500">
         
-          <div className={`${Styles.CardShadow} bg-slate-50 w-5/6 h-full rounded-xl flex flex-col p-6 justify-evenly items-center`}>
+          <div className={`${Styles.CardShadow} bg-slate-50 w-5/6 h-full rounded-xl flex flex-col p-6 justify-evenly items-center max-md:w-full`}>
           <div className=" mr-2  bg-blue-100 flex justify-items-center items-center rounded-full p-2">
               <Image
                 src="/burger.png"
@@ -34,7 +34,7 @@ export default function PopularDishes() {
           </div>
           <div className="card_1 flex justify-center items-center ">
         
-        <div className={`${Styles.CardShadow} bg-slate-50 w-5/6 h-full rounded-xl flex flex-col p-6 justify-evenly items-center`}>
+        <div className={`${Styles.CardShadow} bg-slate-50 w-5/6 h-full rounded-xl flex flex-col p-6 justify-evenly items-center max-md:w-full`}>
         <div className=" mr-2  bg-blue-100 flex justify-items-center items-center rounded-full p-2">
             <Image
               src="/sandwich.png"
@@ -51,7 +51,7 @@ export default function PopularDishes() {
          
         </div><div className="card_1 flex justify-center items-center">
         
-        <div className={`${Styles.CardShadow} bg-slate-50 w-5/6 h-full rounded-xl flex flex-col p-6 justify-evenly items-center`}>
+        <div className={`${Styles.CardShadow} bg-slate-50 w-5/6 h-full rounded-xl flex flex-col p-6 justify-evenly items-center max-md:w-full`}>
         <div className=" mr-2  bg-blue-100 flex justify-items-center items-center rounded-full p-2">
             <Image
               src="/icecream.png"
@@ -68,7 +68,7 @@ export default function PopularDishes() {
          
         </div><div className="card_1 flex justify-center items-center">
         
-        <div className={`${Styles.CardShadow} bg-slate-50 w-5/6 h-full rounded-xl flex flex-col p-6 justify-evenly items-center`}>
+        <div className={`${Styles.CardShadow} bg-slate-50 w-5/6 h-full rounded-xl flex flex-col p-6 justify-evenly items-center max-md:w-full`}>
         <div className=" mr-2  bg-blue-100 flex justify-items-center items-center rounded-full p-1">
             <Image
               src="/juice.png"
