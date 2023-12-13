1b940155aaba284cc1002907f9178a9e029baab7
Ntutu Sekonyela <n.sekonyela@alustudent.com>
2023-12-13 14:29:46 +0200

    fixed the responsive height for small size screens, modified the testimonies

--- pages/Testimonies/testimonies.js
+++ pages/Testimonies/testimonies.js
@@ -5,8 +5,8 @@ import b from "../contact-page/contact";
 export default function Testimonies() {
   return (
     <>
-      <div className="wrapper  flex w-full  h-screen px-20  max-md:px-10 max-md:h-fit max-md:my-40">
-        <div className="left flex flex-col  w-1/2 mt-20  justify-center  max-md:justify-start max-md:mt-0">
+      <div className="wrapper  flex w-full  h-screen px-20  max-md:px-10 max-md:h-fit max-md:my-40 max-sm:flex-col max-sm:px-1  max-sm:items-center">
+        <div className="left flex flex-col  w-1/2 mt-20  justify-center  max-md:justify-start max-md:mt-0 max-sm:w-4/6">
           <div className="flex justify-center  ">
             <Image
               src="/chef.png"
@@ -18,12 +18,12 @@ export default function Testimonies() {
             />
           </div>
         </div>
-        <div className="left flex flex-col   w-1/2 mt-32 max-md:justify-start max-md:mt-0 max-md:pt-7">
+        <div className="left flex flex-col   w-1/2 mt-32 max-md:justify-start max-md:mt-0 max-md:pt-7 max-sm:w-full">
           <h4 className="text-red-500 font-medium mb-6">TESTIMONIALS</h4>
-          <h1 className="text-slate-950 font-bold text-5xl w-5/6  mb-8 max-md:text-3xl max-md:w-full ">
+          <h1 className="text-slate-950 font-bold text-5xl w-5/6  mb-8 max-md:text-3xl max-md:w-full  max-sm:text-2xl max-sm:w-5/6">
             What Our Customers Say About Us
           </h1>
-          <p className="w-4/6 text-slate-500 font-normal mb-8 max-md:w-5/6 max-md:font-semibold">
+          <p className="w-4/6 text-slate-500 font-normal mb-8 max-md:w-5/6 max-md:font-semibold max-sm:w-full">
             “I had the pleasure of dining at Foodi last night, and I'm still
             raving about the experience! The attention to detail in presentation
             and service was impeccable”
