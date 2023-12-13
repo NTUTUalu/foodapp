901db90bf0e4a90ff0dec4f72b388c7a422c8baf
Ntutu Sekonyela <n.sekonyela@alustudent.com>
2023-12-13 15:31:14 +0200

    fixed the responsive height for small size screens, modified the footer

--- pages/footer/footer.js
+++ pages/footer/footer.js
@@ -4,24 +4,24 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <>
-      <div className='flex-col w-full h-80'>
-        <div className=" grid grid-cols-4 gap-4 w-full upper_row h-4/5 justify-evenly px-20 py-12 max-md:px-10">
-          <div className="firstColumn flex-col">
-          <Link href="/" >
+      <div className='flex-col w-full h-80  max-sm:h-72'>
+        <div className=" grid grid-cols-4 gap-4 w-full upper_row h-4/5 justify-evenly px-20 py-12 max-md:px-10 max-sm:grid-cols-1  max-sm:px-1 max-sm:h-fit max-sm:pb-0 max-sm:bg-green-200">
+          <div className="firstColumn flex-col max-sm:justify-items-center max-sm:text-center">
+          <Link href="/" className=" max-sm:w-full max-sm:flex max-sm:justify-center">
           <Image
             src="/foodlogo.png"
             alt="Vercel Logo"
-            className="dark:invert mb-4 max-md:w-20"
+            className="dark:invert mb-4 max-md:w-20 max-sm:justify-self-center"
             width={100}
             height={25}
             priority
           />
         </Link>
-        <p className="w-3/4 text-slate-500 font-medium">
+        <p className="w-3/4 text-slate-500 font-medium max-sm:w-full">
           Savor the artistry where every dish is a culinary masterpiece
         </p>
           </div>
-          <div className="secondColumn flex flex-col  text-slate-500 font-medium text-base"> 
+          <div className="secondColumn flex flex-col  text-slate-500 font-medium text-base max-sm:hidden"> 
             <h2 className="text-lg font-bold text-slate-600">Useful links</h2>
           
             <Link href="#" className=" ">   About us </Link>
@@ -29,22 +29,22 @@ export default function Footer() {
             <Link href="#" className="  ">  Blogs </Link>
             <Link href="#" className="  ">   FAQ </Link>
           </div>
-          <div className="thirdColumn flex flex-col text-slate-500 text-base font-medium">
+          <div className="thirdColumn flex flex-col text-slate-500 text-base font-medium max-sm:hidden">
           <h2 className="font-bold text-lg text-slate-600" >Main Menu</h2>
             <Link href="#" className="">   Home </Link>
             <Link href="#" className="">   Offers </Link>
             <Link href="#" className="">  Menus </Link>
             <Link href="#" className="">   Reservation </Link>
           </div>
-          <div className="forthColumn flex flex-col text-slate-500 text-base font-medium">
-          <h2 className="font-bold text-lg text-slate-600">Contact Us</h2>
+          <div className="forthColumn flex flex-col text-slate-500 text-base font-medium max-sm:w-full  max-sm:text-center">
+          <h2 className="font-bold text-lg text-slate-600 ">Contact Us</h2>
             <Link href="#" className="">   example@gmail.com </Link>
             <Link href="#" className="">+64 958 248 966 </Link>
-            <Link href="#" className="">  Social Media </Link>
+            <Link href="#" className="max-sm:hidden">  Social Media </Link>
             
           </div>
         </div>
-        <div className="lower_row flex w-full  h-2/6 align-middle px-20 max-md:px-10">
+        <div className="lower_row flex w-full  h-2/6 align-middle px-20 max-md:px-10 max-sm:flex-col max-sm:items-center  max-sm:justify-evenly max-sm:bg-green-300">
           <div className="icons flex items-center">
             <div className="mr-2 justify-items-center items-center rounded-full p-1.5 bg-blue-100 max-md:p-1">
             <Image
@@ -87,7 +87,7 @@ export default function Footer() {
           />
        </div>
           </div>
-          <div className="copyrights flex items-center ml-16 text-sm text-slate-600">
+          <div className="copyrights flex items-center ml-16 text-sm text-slate-600 max-sm:w-full max-sm:m-0">
             Copyright 
             <Image
             
