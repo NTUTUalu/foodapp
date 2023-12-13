59a198b2af88107d78bc4ae3fa0ffc1f89196c01
Ntutu Sekonyela <n.sekonyela@alustudent.com>
2023-12-13 14:45:52 +0200

    fixed the responsive height for small size screens, modified the stories

--- pages/stories/stories.js
+++ pages/stories/stories.js
@@ -6,15 +6,15 @@ import Styles from "./stories.module.css";
 export default function Stories() {
   return (
     <>
-      <div className="wrapper  flex w-full  px-20 h-screen max-md:px-10 max-md:h-fit max-md:mb-20">
-        <div className="left flex flex-col  w-1/2 mt-32 max-md:mt-8 ">
+      <div className="wrapper  flex w-full  px-20 h-screen max-md:px-10 max-md:h-fit max-md:mb-20 max-sm:flex-col max-sm:p-1 ">
+        <div className="left flex flex-col  w-1/2 mt-32 max-md:mt-8 max-sm:w-full max-sm:mb-16">
           <h4 className="text-red-500 font-medium mb-6">
             OUR STORY & SERVICES
           </h4>
-          <h1 className="text-slate-950 font-bold text-4xl w-4/6 mb-6 max-md:text-3xl ">
+          <h1 className="text-slate-950 font-bold text-4xl w-4/6 mb-6 max-md:text-3xl max-sm:text-2xl">
             Our Culinary Journey And Services
           </h1>
-          <p className="w-4/6 text-slate-500 font-normal mb-8 max-md:w-5/6">
+          <p className="w-4/6 text-slate-500 font-normal mb-8 max-md:w-5/6 max-sm:w-full">
             Rooted in passion, we curate unforgettable dining experiences and
             offer exceptional services, blending culinary artistry with warm
             hospitality.
@@ -27,7 +27,7 @@ export default function Stories() {
             </Link>
           </div>
         </div>
-        <div className="right w-1/2 grid grid-cols-2 gap-0 max-md:w-4/6 max-md:gap-x-4 max-md:gap-y-10 ">
+        <div className="right w-1/2 grid grid-cols-2 gap-0 max-md:w-4/6 max-md:gap-x-4 max-md:gap-y-10 max-sm:w-full max-sm:h-96">
           <div className="Catering flex justify-center items-center  max-md:items-end ">
             <div
               className={`${Styles.CardShadow} bg-slate-50 w-3/4 h-4/6 rounded-xl flex flex-col p-6 justify-evenly items-center max-md:w-full max-md:h-full max-md:${Styles.CardShadow2} max-md:p-1`}
@@ -40,7 +40,7 @@ export default function Stories() {
                 height={40}
                 priority
               />
-              <h3 className="font-bold text-base text-green-600 ">Catering</h3>
+              <h3 className="font-bold text-base text-green-600 ">CATERING</h3>
               <p className="text-center text-slate-500">
                 Delight your guests with our flavors and presentation
               </p>
